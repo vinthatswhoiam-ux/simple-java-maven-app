ab64531247ce1d6a1b51ea5e9826d0133e1347ab
vinthatswhoiam-ux <vin.thatswhoiam@gmail.com>
2026-06-03 13:50:57 +0000

    App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,21 +1,14 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
-public class App
-{
+public class App {
 
     private final String message = "Hello World!";
-
-    public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
 
-    private final String getMessage() {
+    public String getMessage() {
         return message;
     }
-
 }
